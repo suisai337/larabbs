e508650ed62f950b0593f91b98277e5b1029319f
vagrant <vagrant@homestead>
2019-05-29 15:02:44 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,10 +2,17 @@
 namespace App\Http\Controllers;
 
 
+use App\Model\User;
+
 class UsersController
 {
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
