0940f617ae2fead78d58fb7285056c28af7199c6
Yanfan <330483691@qq.com>
2019-06-04 14:37:55 +0000

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StaticPagesController extends Controller
 {
     public function home()
     {
-        return view('static_pages/home');
+        $feed_items = [];
+        if (Auth::user()) {
+            $feed_items = Auth::user()->feed()->paginate(15);
+        }
+        return view('static_pages/home', compact('feed_items'));
     }
 
     public function help()
